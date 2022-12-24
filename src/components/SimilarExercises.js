742214c8374e6e54c5b9cc91a23ce897a67b4ff4
Javid <goldeniron11@mail.ru>
2022-12-24 15:09:45 +0400

    finishing project

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-const SimilarExercises = () => {
+import { Box, Stack, Typography } from '@mui/material';
+
+import HorizontalScrollbar from './HorizontalScrollbar';
+import Loader from './Loader';
+
+const SimilarExercises = ({ equipmentExercises, targetMuscleExercises }) => {
 	return (
-		<div>SimilarExercises</div>
-	)
-}
+		<Box sx={{ mt: { lg: '100px', xs: '0' } }}>
+			<Typography variant='h3' mb={5}>Exercises that target the same muscle group</Typography>
+			<Stack direction='row' sx={{ p: '2', position: 'relative' }}>
+				{targetMuscleExercises.length ?
+					<HorizontalScrollbar data={targetMuscleExercises} />
+					: <Loader />}
+			</Stack>
 
-export default SimilarExercises
+			<Typography variant='h3' mb={5}>Exercises that use the same equipment</Typography>
+			<Stack direction='row' sx={{ p: '2', position: 'relative' }}>
+				{equipmentExercises.length ?
+					<HorizontalScrollbar data={equipmentExercises} />
+					: <Loader />}
+			</Stack>
+		</Box>
+	);
+};
+
+export default SimilarExercises;
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -12,6 +12,8 @@ import SimilarExercises from '../components/SimilarExercises';
 const ExerciesDetail = () => {
 	const [exerciseDetail, setExerciseDetail] = useState({});
 	const [exerciseVideos, setExerciseVideos] = useState([]);
+	const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+	const [equipmentExercises, setEquipmentExercises] = useState([]);
 	const { id } = useParams();
 
 	useEffect(() => {
@@ -25,6 +27,14 @@ const ExerciesDetail = () => {
 
 			const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
 			setExerciseVideos(exerciseVideosData.contents);
+
+
+			const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+			setTargetMuscleExercises(targetMuscleExercisesData);
+
+			const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+			setEquipmentExercises(equipmentExercisesData);
+
 		};
 
 		fetchExercisesData();
@@ -34,7 +44,7 @@ const ExerciesDetail = () => {
 		<Box>
 			<Detail exerciseDetail={exerciseDetail} />
 			<ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-			<SimilarExercises />
+			<SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
 		</Box>
 	);
 };
