7b685f1abf52382111c174271e599d5e41724f42
Javid <goldeniron11@mail.ru>
2022-12-24 14:42:41 +0400

    exercises detail page

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,15 @@ export const exerciseOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '7704401ac0mshd45ea7c5d1e40a7p156f59jsn47b4cf464515',
-		'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_KEY
+		'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_EXERCISESDB_KEY
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '7704401ac0mshd45ea7c5d1e40a7p156f59jsn47b4cf464515',
+    'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_YOUTUBE_KEY
   }
 };
 
